f91d3674e55efef579661f73c3ac7ed1a13a1abf
Theodore <unawares15@gmail.com>
2022-02-21 12:21:44 +0600

    add fixes

---
+++ lms/envs/mageuni.py
@@ -0,0 +1,14 @@
+from .production import *
+
+
+COURSE_MODE_DEFAULTS = {
+    'bulk_sku': None,
+    'currency': 'vnd',
+    'description': None,
+    'expiration_datetime': None,
+    'min_price': 0,
+    'name': _('Audit'),
+    'sku': None,
+    'slug': 'audit',
+    'suggested_prices': '',
+}
